fcc75d1b6e3e5f81dc36519d424e5072ec7c585e
GUNASEKHAR <gunasekhar.anchala@evry.com>
2017-09-05 16:50:27 +0530

    Created parent and all module projects

---
+++ srv/model/src/main/java/model/Customer.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Customer {
+
+	String name;
+	int number;
+	String palce;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getPalce() {
+		return palce;
+	}
+
+	public void setPalce(String palce) {
+		this.palce = palce;
+	}
+
+}
